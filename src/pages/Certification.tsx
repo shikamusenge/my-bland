ccdfd689e26547273f789021b889dd47a925c98b
shikamusenge <shikamusenge720@gmail.com>
2024-01-23 12:29:04 +0200

    English certificate added

--- src/pages/Certification.tsx
+++ src/pages/Certification.tsx
@@ -5,6 +5,7 @@ const Certification = () => {
   const Award = [
     "TVET certificate V In software Development",
     "Andera Kick-strat program certificate",
+    "English Level 7 certificate EF Education",
   ];
   return (
     <div className="grid min-h-screen items-center p-4 md:px-12">
@@ -21,7 +22,9 @@ const Certification = () => {
         <div className="md:w-9/12 p-4 bg-[#013853] flex md:px-12 text-white items-center">
           <div className="">
             <div>
-              <p className="text-lg md:text-[20px] mb-4 mt-5 tracking-wider">Awards</p>
+              <p className="text-lg md:text-[20px] mb-4 mt-5 tracking-wider">
+                Awards
+              </p>
               <div className="w-full text-[30px]">
                 {Award.map((item, i) => (
                   <p key={i} className="mb-2 flex gap-1 ">
